5743c175065998347ca9799b03c22b23ac5d2cbf
samedfft2634 <samedfft@gmail.com>
2024-04-16 20:08:17 +0300

    feat:token

--- src/routes/index.js
+++ src/routes/index.js
@@ -6,7 +6,8 @@ const router = require('express').Router();
 
 // user:
 router.use('/users', require('./user'))
-
+// token:
+router.use('/tokens', require('./token'))
 
 /* ______________________________________________ */
 module.exports = router 

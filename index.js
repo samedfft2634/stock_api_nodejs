7d6c4e2c6ba0106af8f1ba0a6a8624735315e505
samedfft2634 <samedfft@gmail.com>
2024-05-08 17:13:06 +0300

    feat:remote-database&public-folder

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 "use strict";
 /* ___________________ STOCK API ___________________ */
 const express = require("express");
+const path = require("node:path");
 const app = express();
 
-require("dotenv").config();
+require("dotenv").config({ path: __dirname + "/.env" });
 const HOST = process.env?.HOST || "127.0.0.1";
 const PORT = process.env?.PORT || 8000;
 
 require("express-async-errors");
+
+app.use(express.static(path.join(__dirname, "public")));
+
 
 /* _________________ Connection _________________ */
 const { dbConnection } = require("./src/configs/dbConnection");
@@ -19,6 +23,8 @@ app.use(express.json());
 
 app.use("/upload", express.static("./upload"));
 
+app.use(require("cors")());
+
 app.use(require("./src/middlewares/authentication"));
 
 app.use(require("./src/middlewares/logger"));
@@ -27,25 +33,37 @@ app.use(require("./src/middlewares/queryHandler"));
 
 /* _____________________ Routes ____________________ */
 
-app.all("/", (req, res) => {
+app.all("/api/v1", (req, res) => {
 	res.send({
 		error: false,
 		message: "Welcome to STOCK API",
 		docs: {
-			swagger: "/documents/swagger",
-			redoc: "/documents/redoc",
-			json: "/documents/json",
+			swagger: "/api/v1/documents/swagger",
+			redoc: "/api/v1/documents/redoc",
+			json: "/api/v1/documents/json",
 		},
 		user: req.user,
 	});
 });
 
-app.use(require("./src/routes"));
+app.use('/api/v1',require("./src/routes"));
 
 /* _____________________ OTHERS ____________________ */
 
+app.get("*", (req, res) => {
+	res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+});
+
+app.use("*", (req, res) => {
+	res.status(404).json({ msg: "not found" });
+});
 app.use(require("./src/middlewares/errorHandler"));
 
 app.listen(PORT, HOST, () => console.log(`http://${HOST}:${PORT}`));
+
+// if (process.env.NODE_ENV == "development") {
+// 	require("./src/configs/sync")();
+// }
+  
 // require("./src/helpers/sync")();
 // node swaggerAutogen.js => for swagger Documentations
